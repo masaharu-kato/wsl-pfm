2fa2f552dd27e48b14fd388e5f0d5221a2eefa69
Masaharu Kato <fudai.mk@gmail.com>
2022-11-21 06:58:00 +0900

    fix to only save when ip changed

--- bin/set-ip-to-host.sh
+++ bin/set-ip-to-host.sh
@@ -8,6 +8,11 @@ DIR_DISTRO="$DIR_WSL_PFM/distro/$DISTRO"
 mkdir -p "$DIR_WSL_PFM"
 mkdir -p "$DIR_DISTRO"
 
-my_ip=$(ip a show eth0 | grep -w -m1 inet | awk '{print $2}' | awk -F'/' '{print $1}')
-
-echo -n "$my_ip" > "$DIR_DISTRO/ip"
+c_ip=$(ip a show eth0 | grep -w -m1 inet | awk '{print $2}' | awk -F'/' '{print $1}')
+saved_ip=""
+if [[ -e "$DIR_DISTRO/ip" ]]; then
+    saved_ip=$(cat "$DIR_DISTRO/ip")
+fi
+if [[ "$c_ip" != "$saved_ip" ]]; then
+    echo -n "$c_ip" > "$DIR_DISTRO/ip"
+fi
