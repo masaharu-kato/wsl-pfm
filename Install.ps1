056eda500fda66ef9601d40d3255a785da7bddb1
Masaharu Kato <fudai.mk@gmail.com>
2022-12-28 10:54:03 +0900

    update installer and uninstaller

--- Install.ps1
+++ Install.ps1
@@ -1,37 +1,23 @@
 #Requires -RunAsAdministrator
-
 Param(
-    [string]$TaskName = "Start-WSL-PFM"
+    [alias("d")][string]$Distro,
+    [string]$TaskName = "Start-WSL-PFM",
+    [switch]$noStart
 )
 
-$global:ErrorActionPreference = 'Stop'
-try {
-    $ROOT_DIR = Resolve-Path "$(Split-path -parent $MyInvocation.MyCommand.Definition)"
+$ROOT_DIR = Resolve-Path "$(Split-path -parent $MyInvocation.MyCommand.Definition)"
 
-    $WSL_PFM_SCRIPT_PATH = Resolve-Path "$ROOT_DIR\host\Start.ps1"
+# Register a Scheduled Task
+. $ROOT_DIR/host/Register-ScheduledTask.ps1 -TaskName $TaskName
 
-    if(Get-ScheduledTask -TaskName $TaskName -ErrorAction Ignore) {
-        Write-Host "Notice: Scheduled Task '$TaskName' already exists"
-    }
+# Install on WSL
+. $ROOT_DIR/host/Install-on-WSL.ps1 -d $Distro
 
-    # Trigger: Run at host (windows) startups
-    $Trigger = New-ScheduledTaskTrigger -AtStartup
-
-    # Action: Run WSL-PFM host script
-    $Action = New-ScheduledTaskAction -Execute powershell -Argument "-ExecutionPolicy RemoteSigned -File `"$WSL_PFM_SCRIPT_PATH`""
-
-    # Task settings
-    $TaskSet = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden
-
-    # Task principles (Run as SYSTEM without logon)
-    $Principal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId SYSTEM
-
-    # Register
-    Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -Settings $TaskSet -Principal $Principal -Force | Out-Null
-
-    Write-Host "Successfully installed as a Scheduled Task '$TaskName'" -ForegroundColor Green
-
-} catch {
-    Write-Host "Failed to Install." -ForegroundColor Red
-    Write-Host $_ -ForegroundColor Magenta
+if (!$noStart) {
+    # Start a Scheduled Task
+    Start-ScheduledTask -TaskName $TaskName
+    Write-Host "Successfully started WSL Port Forwarding Manager." -ForegroundColor Green
+}
+else {
+    Write-Host "Restart a host computer to start WSL Port Forwarding Manager." -ForegroundColor Yellow
 }
